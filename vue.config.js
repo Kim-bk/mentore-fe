ace98bccc05ebf7c3a51fcc03c3dbb266c998c08
Kim Nguyen <nguyenhoangkim120201@gmail.com>
2023-07-07 21:44:52 +0700

    deploy

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,9 @@
 module.exports = {
   devServer: {
-    proxy: 'http://localhost:41783'
-    }
-  }
+    proxy: "http://localhost:41783",
+  },
+  publicPath:
+    process.env.NODE_ENV === "production"
+      ? "/mentore-fe/" // Thay tên repository của các bạn vào đây nhé
+      : "/",
+};
